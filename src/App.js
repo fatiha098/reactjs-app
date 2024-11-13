1dc221789e30d60776de28ed04c7a369929da68b
fatiha098 <155731772+fatiha098@users.noreply.github.com>
2024-11-13 13:16:42 +0100

    vesrion 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import image from './images/owl.png';
+import image2 from './images/owl-sans-bg.png';
+
 
 function App() {
+
+  const [result, setResult] = useState('');
+
+  const onClickBtn = (e) => {
+
+    e.preventDefault();
+
+    const input = document.getElementById('name')
+    const span =  document.querySelector('.warning');
+
+    const name = input.value;
+
+    if(name === ''){
+
+      input.style.border = '2px solid red';
+      span.style.visibility = "visible";
+      span.textContent = 'Please enter a name';
+      return 
+    }
+
+    getData(name);
+  }
+
+  const getData = async (name) => {
+
+    const input = document.getElementById('name')
+    const span =  document.querySelector('.warning');
+
+    const url = `https://api.agify.io/?name=${name}`;
+
+    const resp = await axios.get(url);
+
+    if(resp.data.age === null ){
+
+      input.style.border = '2px solid red';
+      span.style.visibility = "visible";
+      span.textContent = 'Name is not valid';
+
+    }else {
+
+      setResult(`${name}'s age is ${resp.data.age} years old`);
+      document.querySelector('.result').style.animation = "zoom 0.4s"
+    }
+  }
+
+  const handleClick = () => {
+
+    const switchBtn = document.querySelector('.switchBtn');
+    const switchParent = document.querySelector('.switch');
+    const body = document.querySelector('.body');
+    const input = document.querySelector('.input');
+    const owl = document.querySelector('.owl');
+    const result = document.querySelector('.result');
+
+    switchBtn.classList.toggle('light');
+    body.classList.toggle('dark');
+    switchParent.classList.toggle('switch-btn-dark');
+    switchBtn.classList.toggle('transition');
+    input.classList.toggle('input-border')
+    result.classList.toggle('white-color');
+    owl.classList.toggle('hide-image');
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className='body'>
+    <div className="switch"><div className="switchBtn" onClick={handleClick}></div></div>
+    <div className="content">
+    <img src={image} alt="not found" className='owl' ></img>
+    <img src={image2} alt="not found" className='owl'></img>
+    
+    <form action="">
+      <input type="text" name="name" placeholder="Enter a name" id="name" className="input" ></input>
+        <br></br>
+      <div className="btn">
+        <div>
+          <input type="submit" value="Click Me" id="click" onClick={onClickBtn}></input>
+          <span className="warning">message</span>
+          </div>
+      </div>
+    </form>
+    <div className="result">{result}</div>
     </div>
+  </div>
   );
 }
 
